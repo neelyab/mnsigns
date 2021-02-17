ce944398793ecb435e223a43e8e7cc9efa17c788
unknown <neelyab@vcu.edu>
2021-02-16 20:01:49 -0500

    added styling to product page

--- src/Products/ProductData.js
+++ src/Products/ProductData.js
@@ -3,28 +3,28 @@ export const ProductData = [
         id: 1,
         title: 'Vehicle Wraps',
         thumbnail: 'https://i.imgur.com/1kbeMW7.jpg',
-        images: [],
+        images: ['https://i.imgur.com/mQY7NiJ.jpg', 'https://i.imgur.com/TdGND2J.jpg', 'https://i.imgur.com/6miK6zE.jpg', 'https://i.imgur.com/Aguyta8.jpg', 'https://i.imgur.com/41hNBt6.jpg', 'https://i.imgur.com/hWrwOEg.jpg'],
         description:''
     },
     {
         id: 2,
         title: 'Yard Signs',
-        thumbnail: '',
-        images: [],
+        thumbnail: 'https://i.imgur.com/NoywXux.jpg',
+        images: ['https://i.imgur.com/8y9gIoj.jpg', 'https://i.imgur.com/WWuuRWb.jpg', 'https://i.imgur.com/sHFYItc.jpg', 'https://i.imgur.com/wxze8a3.jpg', 'https://i.imgur.com/Pe3vEGQ.jpg'],
         description:''
     },   
     {
         id: 3,
         title: 'Banners',
         thumbnail: 'https://i.imgur.com/6OLaSfx.jpg',
-        images: [],
+        images: ['https://i.imgur.com/WigS1il.jpg', 'https://i.imgur.com/ukOWnn6.jpg'],
         description:''
     },
     {
         id: 4,
         title: 'Panel Signs',
         thumbnail: 'https://i.imgur.com/cDwSAg3.jpg',
-        images: [],
+        images: ['https://i.imgur.com/GOMwMjN.jpg', 'https://i.imgur.com/3tldUsB.jpg', 'https://i.imgur.com/r9xUepo.jpg'],
         description:''
     },
     {
--- src/Products/Products.js
+++ src/Products/Products.js
@@ -8,12 +8,14 @@ class Products extends Component {
     render(){
         const productList = ProductData.map(product => {
             return(<li className="product-category" key={product.id}>
-                <Link to={`/product/${product.id}`}>
-                    {product.title}
-                </Link>
+                <div className="text-container">
                 <Link to={`/product/${product.id}`}>
                 <img className="product-thumbnails" src={product.thumbnail} alt={product.title} />
                 </Link>
+                <Link to={`/product/${product.id}`}>
+                <div className="centered-text">{product.title}</div>
+                </Link>
+                </div>
             </li>)
         })
         return(
